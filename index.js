c640902e39c0a8ae547e39b02a835c32c006599f
sreevidya-alluri <sreevidya.alluri@gmail.com>
2024-08-06 15:48:09 +0530

    new

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 const userRoutes = require("./routes/userRoutes")
 const pokemonRoutes = require('./routes/pokemonRoutes'); 
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://apexplusfrontendsreevidya.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use(bodyParser.json()); 
 app.use('/api/pokemons', pokemonRoutes); 
